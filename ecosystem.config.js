bb7c32bd175f6dc02426dba7830949fabc493cb9
Eklou Amemassovor <amekley@gmail.com>
2024-05-19 19:24:42 +0000

    ecosystem config added

---
+++ ecosystem.config.js
@@ -0,0 +1,27 @@
+// ecosystem.config.js
+
+module.exports = {
+  apps: [
+    {
+      name: 'andsomore',
+      script: 'node_modules/.bin/next',
+      args: 'start',
+      cwd: '/home/asm',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+        NEXT_PUBLIC_URL: 'https://andsomore.com',
+        NEXTAUTH_URL: 'https://andsomore.com',
+        // NEXTAUTH_SECRET: 'sdf',
+        // MONGO_URI: 'mongodb://localhost:27017/chai',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+}
